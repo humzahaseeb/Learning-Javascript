96cbd29aea8a1db773bf838102771580eb012f4e
Hamza Haseeb <hamzahaseeb5@gmail.com>
2020-12-06 19:22:50 +0500

    Further Practice

---
+++ Coding Challenge 4/script.js
@@ -0,0 +1,34 @@
+var Mark = {
+    fullName: 'Mark Zuckerberg',
+    mass: 90,
+    height: 1.5,
+    bmiMark : function(){  
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+
+
+var John = {
+    fullName: 'John Smith',
+    mass: 70,
+    height: 1.95,
+    bmiJohn : function(){  
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+Mark.bmiMark();
+John.bmiJohn();
+
+console.log(Mark, John);
+
+
+
+
+
+
+
+
---
+++ Objects/objects.js
@@ -0,0 +1,41 @@
+var hamza = {
+    firstName: 'Hamza',
+    LastName: 'Haseeb',
+    birthYear: 1999,
+    degree: 'Computer Science',
+    university: 'University of Engineering and Technology, Lahore',
+    favTeam: 'FC Barcelona',
+    favPlayer: 'Lionel Messi',
+    family: ['Tayyab','Ahmed' ],
+    isMarried: false,
+    calcAge: function(birthYear){
+        this.age = 2020 - this.birthYear;
+    }
+};
+
+console.log(hamza);
+
+/*
+var tayyab = {
+    firstName: 'Tayyab',
+    LastName: 'Haseeb',
+    age: 20,
+    degree: 'Mechanical Engineering ',
+    university: 'NFC Institute, Faisalabad',
+    favTeam: 'Real Madrid',
+    favPlayer: 'Cristiano Ronaldo',
+    family: ['Hamza','Ahmed' ],
+    isMarried: false
+};
+
+*/
+
+
+
+
+
+
+
+
+
+
