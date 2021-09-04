aa011ee30022e4a704e9434b4a70405cfbfc2b71
Humza Haseeb <hamzahaseeb5@gmail.com>
2021-09-04 21:48:49 +0500

    Update script.js

--- Coding Challenge 3 (Tip Calculator)/script.js
+++ Coding Challenge 3 (Tip Calculator)/script.js
@@ -16,12 +16,12 @@ else if (bill > 200){
             
 let bills = [124, 48, 268];
 
-var tips = [tipCalculator(bills[0]), 
+let tips = [tipCalculator(bills[0]), 
             tipCalculator(bills[1]),
             tipCalculator(bills[2])
             ];
 
-var totalAmountPaid = [bills[0] + tips[0],
+let totalAmountPaid = [bills[0] + tips[0],
                       bills[1] + tips[1],
                       bills[2] + tips[2]];
 
